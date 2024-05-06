a86268ac3c901817527546a85a806ce949f2d56a
Reko <36913@ma-web.nl>
2024-05-06 10:20:14 +0200

    opdr 1 echt

---
+++ ConsoleMonGame/ConsoleMon/ConsoleMon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
