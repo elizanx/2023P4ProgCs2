4f7eaf4554ba6b4b00c6bfca5dabb66eb55ce5b5
Reko <36913@ma-web.nl>
2024-05-24 15:22:39 +0200

    opdr7

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            Colors myMainThemeColor = Colors.Red;
+
+            if (myMainThemeColor == Colors.Red) 
+            {
+                Console.WriteLine("Dat is mijn kleur!");     
+            }
+        }
+
+    }
+}
