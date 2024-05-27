fbcc5c8c534eb0c48840fb3305f3808e0074ece9
Reko <36913@ma-web.nl>
2024-05-27 10:44:46 +0200

    opdr9

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\MA\periode 4\PROG\2023P4ProgCs2");
+
+
+            FileInfo[] files = dir.GetFiles();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine(file.FullName);
+            }
+
+            
+        }
+    }
+}
