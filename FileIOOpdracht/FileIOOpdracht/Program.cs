ebf0ebb2e5498468a621448d91d12e9803c49451
eliza <elizanabibaks@gmail.com>
2024-06-21 15:08:45 +0200

    opdr10

---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string relDir = new FileInfo(typeof(Program).Assembly.Location).Directory.FullName;   
+            string path = Path.Combine(relDir, "leesdezefile.txt");
+            string[] content = File.ReadAllLines(path);
+            
+            string path2 = Path.Combine(relDir, "mijnnieuwefile.txt");
+            string[] content2 = File.ReadAllLines(path2);
+
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                Console.WriteLine(content[i]);
+            }
+
+
+            
+            File.WriteAllText("mijnnieuwetextfile.txt", "nieuwe text"); 
+            File.AppendAllText("mijnnieuwetextfile.txt", "nieuwe text");
+            string path3 = Path.Combine(relDir, "mijnnieuwefile.txt");
+            string[] content3 = File.ReadAllLines("mijnnieuwetextfile.txt");
+            Directory.CreateDirectory("output");
+         
+
+            for (int i = 0;i < content3.Length; i++)
+            {
+                Console.WriteLine(content3[i]);
+            }
+        }
+    }
+}
